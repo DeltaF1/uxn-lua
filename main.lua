ec9bcddd9b2b989a2b9c87f41f72d1332456e591
DeltaF1 <delyaerricson@gmail.com>
2021-09-28 15:21:50 -0400

    Clean up main.lua
    - Added variables to control screen size
    - Make love.load cleaner

--- main.lua
+++ main.lua
@@ -1,19 +1,27 @@
-PROFILE = nil
-oprint = print
-Uxn = (require "uxn").Uxn
+local Uxn = (require "uxn").Uxn
 bit = require "bit"
 
 local band, bor, bxor, bnot = bit.band, bit.bor, bit.bxor, bit.bnot
 local arshift, rshift, lshift = bit.arshift, bit.rshift, bit.lshift
 
 local math = require "math"
-devices = require "love-devices"
+local devices = require "love-devices"
 
 Device = require "device"
 
+-- Debugging
+--jit.off()
+PROFILE = nil
+
+-- Screen parameters
+WIDTH = 300
+HEIGHT = 200
+PADDING = 20
+SCALING = 2
+
 function setupCPU(mem)
   local cpu = Uxn:new(mem)
-  cpu.ip = 256
+  cpu.ip = 0x0100
   cpu.PRINT = false
   cpu.memory.ERROR_ON_UNINITIALIZED_READ = false
 
@@ -22,31 +30,56 @@ function setupCPU(mem)
   screen = cpu:add_device(2, devices.screen)
   controller = cpu:add_device(8, devices.controller)
   mouse = cpu:add_device(9, devices.mouse)
+  file = cpu:add_device(10, devices.file)
 
   return cpu
 end
 
-function love.load()
-  love.profiler = require('profile.profile')
-  love.graphics.setDefaultFilter( "nearest", "nearest"  )
- 
-  love.graphics.setBackgroundColor(1,1,1)
-  love.graphics.setBackgroundColor(love.math.random(), love.math.random(), love.math.random())
-  cpu = setupCPU()
+function love.load(arg)
+  love.mouse.setVisible(false)
+  love.profiler = require('cigumo_profile')
+  love.graphics.setDefaultFilter("nearest", "nearest")
 
-  local data = love.filesystem.read("data", "boot.rom")
+  love.graphics.setNewFont("mono.ttf", 14)
+  love.graphics.setBackgroundColor(0,0,0)
+  love.window.setMode((WIDTH * SCALING) + (PADDING * 2), (HEIGHT * SCALING) + (PADDING * 2))
+  
+  -- This is the shader that translates system colours into
+  -- palette colours
+  paletteShader = love.graphics.newShader [[
+    uniform vec3 palette[4];
+    
+    vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords ) {
+      vec4 pixel = Texel(tex, texture_coords);
+      int index = int(pixel.r+(pixel.g*2));
+      pixel.rgb = palette[index];
+        
+      return pixel * color;
+    }
+  ]]
 
-  data = love.data.encode("string", "hex", data)
 
-  -- preload memory
+  local memory = {}
+
+  -- Preload the zero-page
   for i = 0, 255 do
-    cpu.memory[i] = 0
-  end
-  for i = 1, #data, 2 do
-    byte = tonumber(string.sub(data, i, i+1), 16)
-    cpu.memory[math.ceil(i/2)+255] = byte
+    memory[i] = 0
   end
 
+  -- Takes in a filename from the command line, else load boot.rom
+  local data, size = love.filesystem.read("data", arg[1] or "boot.rom")
+
+  -- Unpack the bytes into a Lua table
+  data = {love.data.unpack(string.rep("B", size), data)}
+
+  for i = 1, #data - 1 do
+    memory[i + 255] = data[i]
+  end
+
+  -- Create a new CPU
+  cpu = setupCPU(memory)
+
+  -- Execute the initial vector
   cpu:runUntilBreak()
   print("cpu is done initial run") 
 
@@ -61,16 +94,68 @@ function love.load()
   end
 end
 
---[[
-function love.textinput(text)
-  for i = 1, #text do
-    local char = string.byte(text, i)
-    console[2] = char
+local frame = 0
+function love.draw()
+  frame = frame + 1
 
-    cpu:trig_device(1)
+  -- Run the screen vector
+  screen:trigger()
+
+  if frame == PROFILE then
+    love.profiler.stop()
+    print(love.profiler.report())
+    print("device", "num_triggers")
+    for k, v in pairs(cpu.device_triggers) do
+      print(k, v)
+    end
+
+    print("device", "cpu_reads")
+    for k, v in pairs(cpu.device_reads) do
+      print(k, v)
+    end
+
+    print("device", "cpu_writes")
+    for k, v in pairs(cpu.device_writes) do
+      print(k, v)
+    end
+
+    print("device", "total Device reads")
+    for k, v in pairs(Device.DEBUG_NUM_CALLS.read) do
+      print(k, v)
+    end
+
+    print("device, total Device writes")
+    for k, v in pairs(Device.DEBUG_NUM_CALLS.write) do
+      print(k, v)
+    end
+
+    print(cpu:print_profile())
   end
+
+  love.graphics.push()
+
+  love.graphics.translate(PADDING, PADDING)
+  love.graphics.scale(SCALING, SCALING)
+  love.graphics.setColor(1,1,1)
+
+  love.graphics.setBlendMode("replace", "premultiplied")
+  love.graphics.setShader(paletteShader)
+
+  love.graphics.draw(screen.back)
+
+  love.graphics.setBlendMode("alpha")
+  love.graphics.draw(screen.front)
+
+  love.graphics.setShader()
+
+  love.graphics.pop()
+
+  local dbg = "x: "..screen:readShort(8).." y: "..screen:readShort(10).." "
+  dbg = dbg.."PS: "..table.concat(cpu.program_stack, " ").." "
+  dbg = dbg.."frame: "..frame.." fps: "..love.timer.getFPS()
+  love.graphics.setColor(1,0,0)
+  --love.graphics.print(dbg, 10, 300)
 end
-]]--
 
 -- Take in a love keyconstant and return which bit in the controller byte
 keyToBit = {
@@ -121,67 +206,7 @@ function love.keyreleased(key)
   controller:trigger()
 end
 
-frame = 0
-function love.draw()
-  frame = frame + 1
-  print("\n=====[draw]=====\n")
-  screen:trigger()
-  print("draw vector done")
-  if frame == PROFILE then
-    love.profiler.stop()
-    oprint(love.profiler.report())
-    print("device", "num_triggers")
-    for k, v in pairs(cpu.device_triggers) do
-      print(k, v)
-    end
-
-    print("device", "cpu_reads")
-    for k, v in pairs(cpu.device_reads) do
-      print(k, v)
-    end
-
-    print("device", "cpu_writes")
-    for k, v in pairs(cpu.device_writes) do
-      print(k, v)
-    end
-
-    print("device", "total Device reads")
-    for k, v in pairs(Device.DEBUG_NUM_CALLS.read) do
-      print(k, v)
-    end
-
-    print("device, total Device writes")
-    for k, v in pairs(Device.DEBUG_NUM_CALLS.write) do
-      print(k, v)
-    end
-
-    print(cpu:print_profile())
-  end
-  love.graphics.push()
-  love.graphics.scale(2,2)
-  love.graphics.setColor(1,1,1)
-
-  love.graphics.setBlendMode("alpha", "premultiplied")
-
-  love.graphics.draw(screen.back)
-  love.graphics.draw(screen.front)
-
-  love.graphics.setBlendMode("alpha")
-  
-  love.graphics.pop()
-
-
-  local dbg = "x: "..screen:readShort(8).." y: "..screen:readShort(10).." "
-  dbg = dbg.."PS: "..table.concat(cpu.program_stack, " ").." "
-  dbg = dbg.."frame: "..frame.." fps: "..love.timer.getFPS()
-
-  love.graphics.setColor(1,0,0)
-  love.graphics.print(dbg, 10, 300)
-
-  love.graphics.setColor(0,1,1)
-  love.graphics.print(console.stdout, 10, 310)
-end
-
+--[[ TODO
 function love.filedropped(file)
   -- TODO: Reset devices
   cpu = setupCPU() 
@@ -196,27 +221,24 @@ function love.filedropped(file)
 
   cpu:runUntilBreak()
 end
+]]--
 
-local offX = 0
-local offY = 0
-
-local xScale = 2
-local yScale = 2
 function normalizeMouse(x, y)
   -- Move to the center to offset math.floor
   x = x + 0.5
   y = y + 0.5
 
-  x = x - offX
-  y = y - offY
+  x = x - PADDING
+  y = y - PADDING
 
-  x = x / xScale
-  y = y / yScale
+  x = x / SCALING
+  y = y / SCALING
 
   return math.floor(x), math.floor(y)
 end
 
 local old_x, old_y
+
 function love.mousemoved(x, y)
   x, y = normalizeMouse(x, y)
   if x ~= old_x or y ~= old_y then
